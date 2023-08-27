ed834e05039a4fbf99c16b103917b806e2813f2b
Saurav Kumar Roy <136881235+Sauravroy34@users.noreply.github.com>
2023-08-27 14:20:48 +0530

    Add files via upload

--- task_03/c/c.c
+++ task_03/c/c.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+
+int is_prime(int num) {
+    if (num <= 1) {
+        return 0; // 0 and 1 are not prime numbers
+    }
+    
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return 0; // num is divisible by i, so it's not a prime
+        }
+    }
+    
+    return 1; // num is prime
+}
+
+int main() {
+    int n;
+    printf("Enter a number (n): ");
+    scanf("%d", &n);
+    
+    printf("Prime numbers up to %d:\n", n);
+    
+    for (int i = 2; i <= n; ++i) {
+        if (is_prime(i)) {
+            printf("%d ", i);
+        }
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
 
