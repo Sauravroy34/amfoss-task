aa645b99858c0dc18200f3c9ae2688840e2f7630
Saurav Kumar Roy <136881235+Sauravroy34@users.noreply.github.com>
2023-08-27 14:18:42 +0530

    Add files via upload

--- task_03/javascript/javascript.js
+++ task_03/javascript/javascript.js
@@ -1,1 +1,32 @@
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function findPrimes(n) {
+  const primes = [];
+
+  for (let i = 2; i <= n; i++) {
+    if (isPrime(i)) {
+      primes.push(i);
+    }
+  }
+
+  return primes;
+}
+
+const n = prompt("Enter a number: ");
+
+const primes = findPrimes(n);
+
+console.log("The prime numbers up to " + n + " are: " + primes);
 
